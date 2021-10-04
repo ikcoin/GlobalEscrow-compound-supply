79e101abb537f8d7500bcb05bdfb0f752423f3b6
ikcoin <ikcoin.kb@protonmail.com>
2021-10-04 17:42:57 +0200

    cEth contract redeem

--- LFGlobalEscrow.sol
+++ LFGlobalEscrow.sol
@@ -128,8 +128,6 @@ contract LFGlobalEscrow is Ownable {
         //Ether supplying to Compound at the point of deposit:
         CEther cEthToken = CEther('0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5'); //contract address, the cEth contract address
         uint mintResult = cEthToken.mint(e.fund); //num Tokens to supply
-
-
     }
 
     function release(string memory _referenceId) public multisigcheck(_referenceId) {
@@ -183,5 +181,13 @@ contract LFGlobalEscrow is Ownable {
         e.fund = e.fund - _amount;
         e.lastTxBlock = block.number;
         require((e.owner).send(_amount));
+
+        //Ether retrieved only at the point of withdrawal:
+        CEther cEthToken = CEther('0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5'); //contract address, the cEth contract address
+        uint redeemResult = cEthToken.redeem(_amount); //num Tokens to redeem
+
+        // REDEEM_ERROR_CODES
+        emit MyLog("If this is not 0, there was an error", redeemResult);
+        require(redeemResult == 0, "redeemResult error"); //uint 0=success, otherwise a failure
     }
 }
