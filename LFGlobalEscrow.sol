27569c0c8f2f521315059d334d87be90ead6340d
ikcoin <ikcoin.kb@protonmail.com>
2021-10-05 17:00:22 +0200

    Code form

--- LFGlobalEscrow.sol
+++ LFGlobalEscrow.sol
@@ -205,6 +205,7 @@ contract LFGlobalEscrow is Ownable {
         //Ether supplying to Compound at the point of deposit:
         CEther cEthToken = CEther("0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5"); //contract address, the cEth contract address
         uint256 mintResult = cEthToken.mint(e.fund); //num Tokens to supply
+        //mintResult = balance of cEth
     }
 
     function release(string memory _referenceId)
